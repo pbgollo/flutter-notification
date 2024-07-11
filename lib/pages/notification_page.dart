4aaa47dfdf43ae9d27d0734aba4caf1739946c5f
Pedro <pedrobgollo@gmail.com>
2024-07-11 12:32:35 -0300

    Pequenos ajustes

--- lib/pages/notification_page.dart
+++ lib/pages/notification_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class NotificationPage extends StatelessWidget {
   const NotificationPage({super.key});
@@ -11,14 +12,41 @@ class NotificationPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Notificações"),
+        title: Row(
+          children: [
+            Text("Notificações",
+              style: GoogleFonts.bebasNeue(
+                fontSize: 25,
+                fontWeight: FontWeight.w400, 
+                color: Colors.grey[600], 
+              ),
+            ),
+            const SizedBox(width: 10),
+            Icon(Icons.notifications_active_outlined,
+              size: 28,
+              color: Colors.grey[600],
+            ),
+          ],
+        ),
       ),
-      body: Column(
-        children: [
-          Text(message.notification!.title.toString()),
-          Text(message.notification!.body.toString()),
-          Text(message.data.toString()),
-        ],
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 25),
+            Text(message.notification!.title.toString(),
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 5),
+            Text(message.notification!.body.toString(),
+              style: const TextStyle(
+                fontSize: 15,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
